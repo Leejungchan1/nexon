39f8140a3674c44068486aa3b42a9e2fe956b1fa
Leejungchan1 <wjdcks0123@naver.com>
2026-06-29 19:17:21 +0900

    ver1

---
+++ js/nacwon.js
@@ -0,0 +1,38 @@
+$(function(){
+    //메뉴
+
+    $(".nav > a").click(function(){
+        $(".h_inner .nav .m_menu").stop().slideDown();
+        $(".modal").fadeIn();
+    })
+
+    $(".t_link > a").click(function(){
+        $(".h_inner .nav .m_menu").stop().slideUp();
+        $(".modal").fadeOut();
+    })
+
+
+    //미디어
+    $(".move_img").slick({
+        slidesToShow: 1,
+        dots: true,
+        arrows: true,
+        centerMode: true,
+        centerPadding: "330px",
+        responsive:[
+            {
+                breakpoint: 1280,
+                settings:{
+                    slidesToShow: 1,
+                    centerPadding: "50px",
+                }
+            }
+        ]
+    })
+
+    $(".pop_click div").click(function(){
+        $(".popup").hide();
+    })
+
+
+})
